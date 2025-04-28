c7585946a2b7e215da52bd8df371306763015ab4
Marcus Winbush <mwinbush00@gmail.com>
2025-04-28 19:50:50 -0400

    developing main page

--- src/app/Components/Main.tsx
+++ src/app/Components/Main.tsx
@@ -3,10 +3,16 @@
 const Main = () => {
 
 return(
-<div className="flex relative m-2">
-    <h2>
-    Welcome. My name is Marcus
+<div className=" relative m-2">
+    <h2 className="flex font-bold text-4xl">
+    Welcome 
     </h2>
+    <h1>
+    My name is Marcus
+    </h1>
+    <a>
+        I am an aspiring Web Developer with a background in Science. 
+    </a>
     <div>
         Projects
     </div>
