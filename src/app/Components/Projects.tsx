4cc771c38cac52014a45165996a49bcd51de42e6
Marcus Winbush <mwinbush00@gmail.com>
2025-06-19 07:48:07 -0400

    updating project section

--- src/app/Components/Projects.tsx
+++ src/app/Components/Projects.tsx
@@ -5,8 +5,14 @@ import Anime from "../images/anime-api-sc.png";
 import Travel from "../images/travel sc.png";
 import Ecommerce from "../images/ecommerce-sc.png";
 import Weather from '../images/weather-app.png';
+import { useState } from "react";
 
 const Projects = () => {
+  const [showTextBoxOne, setShowTextBoxOne] = useState(false);
+  
+  const handleButtonClickOne = () => {
+    setShowTextBoxOne(true);
+  };
 
 return(
 <div className=" w-full absolute bg-gray-100">
@@ -19,17 +25,52 @@ return(
     </div>
 
     <div className="pt-32 grid  lg:grid-cols-2">
-        <section className="flex  justify-center pb-10 px-4 text-center">
-          <div className="bg-black text-white  shadow-md dark:bg-gray-800 dark:border-gray-700">
-            
+        <section className="flex flex-col justify-center pb-10 px-4 text-center">
+         
+         <div className="bg-black text-white  shadow-md dark:bg-gray-800 dark:border-gray-700">
             <Image
               src={Anime}
               className="w-full h-auto max-w-7xl rounded-t-md"
               alt="Anime"
               loading="lazy"
             />
-
             </div>
+
+         <div className="w-full pt-4">
+          <div className="button-wrapper">
+          <button className="service-button" onClick={handleButtonClickOne}>
+            
+            <span className="font-medium">Live Demo</span>
+            
+            </button>
+          <button className="description-button">Description</button>
+          </div>
+          
+          
+         </div>
+          
+ {
+    <div className="flex justify-center items-center">
+      {showTextBoxOne && (
+        <div className="fixed z-10 inset-0 bg-black/30 flex justify-center items-center text-black px-8">
+          <div className="bg-white rounded-lg p-8 max-w-3xl">
+            <h1 className="text-xl mb-4">Molecular Biology</h1>
+            <p>At Elemental Analytics, we specialize in providing cutting-edge molecular biology solutions tailored to meet the diverse needs of researchers, academic institutions, and biotech organizations. Our mission is to empower scientific discovery and innovation through precise, reliable, and efficient molecular biology services. Some key services we provide are DNA/RNA sequencing, PCR/qPCR analysis, Protein expression and CRISPR.</p>
+            <button 
+            className="mt-4 text-white bg-gradient-to-br from-orange-400 via-red-500 to-orange-400 hover:bg-gradient-to-br shadow-lg shadow-red-400/50 animate-text font-bold py-2 px-4 rounded
+            "
+            onClick={() => setShowTextBoxOne(false)}
+            >
+            Close
+            </button>
+          </div>
+        </div>
+      )}
+
+    </div>
+  }
+           
+       
             
         </section>
 
@@ -65,7 +106,7 @@ return(
 
 
  <section className="flex justify-center pb-10 px-4 text-center">
-                  <div className= "text-white  shadow-md dark:bg-gray-800 dark:border-gray-700">
+                  <div className= " bg-black text-black shadow-md dark:bg-gray-800 dark:border-gray-700">
                     
                     <Image
                       src={Weather}
@@ -73,10 +114,9 @@ return(
                       alt="Anime"
                       loading="lazy"
                     />
-        
                     </div>
-                    
-                </section>
+
+   </section>
 
     </div>
     
