d58fc2d91827064068200114f43a71f8f7fa16ee
Marcus Winbush <mwinbush00@gmail.com>
2025-09-15 17:27:26 -0400

    updating form

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,58 +1,44 @@
 "use client";
-import { useState, FormEvent } from 'react';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import '/src/app/globals.css'
 
 
 
-const Footer = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
 
-    const onSubmit = (e: FormEvent) => {
-        e.preventDefault()
-        console.log('Data', name, email, message)
-    }
-    
+const Footer = () => {
+  
 return (
 <div className='bg-gray-100 px-5 py-5 relative w-full '>
 
 <div className='flex justify-center'>
     <div className='' >
- <div>
-        Contact
+ <h1 className='font-semibold text-3xl'>
+         Feel Free To Contact Me
+    </h1>
+    <div className='icons'>
+        <div className='github'>
+            <FaGithub />
+            </div>
+        <div className='linkedin'>
+            <FaLinkedin/>
+            </div>
+        <div className='mail'>
+            <FaEnvelope/>
+            </div>
+
     </div>
+    
+
     <div className='flex'>
-        <form className='flex flex-col gap-4' onSubmit={onSubmit}>
-            <input 
-            value={name}
-            onChange= {(e) => setName(e.target.value)}
-            type="text"  
-            placeholder='Name' 
-            className='border p-2'></input>
+       
 
-            <input 
-           value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type="email" 
-            placeholder='Email' 
-            className='border p-2' ></input>
-
-            <textarea
-           value={message}
-            onChange={(e) => setMessage(e.target.value)}>
-            </textarea>
-            
-            <button type="submit">Submit</button>
-
-        </form>
     </div>
 <div>
     Footer
 </div>
-    </div>
+    
    
-
+</div>
 </div>
 
 </div>
